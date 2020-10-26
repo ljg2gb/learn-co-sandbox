48b06143ecd7726cda19df13fadd6080a8ba5d50
Lydia Gregory <lydia.gregory.dev@gmail.com>
2020-10-26 17:24:57 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+# Don't do this:
+#puts (1 #+1)
+
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
